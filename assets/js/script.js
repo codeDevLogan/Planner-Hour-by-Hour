b64e7e9250609f07c5f906f1ad3c8d469c6c5550
codeDevLogan <logandevscode@gmail.com>
2023-01-06 21:18:30 -0700

    Worked on implementing jQuery with dayjs and bootstrap. Next step: Work of saving data from textarea to the local storage and get the current day to print across the top of the screen.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+let currentHour = dayjs().hour();
+console.log(currentHour);
+let isPM = false;
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -8,6 +11,63 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+
+  $root = $('#root');
+  $timesheet = $('#timesheet')
+  for(let i = 1; i<25; i++){
+    if(i>12){
+      isPM = false;
+    }else{
+      isPM = true;
+    }
+    let $hour = $(`<div id="hour-${i}">`);
+    if(i === currentHour){
+      $hour.attr('class', 'row time-block present');
+    }else if (i > currentHour){
+      $hour.attr('class', 'row time-block future');
+    }else{
+      $hour.attr('class', 'row time-block past');
+    }
+    let $time = $('<div>')
+    $time.attr('class', 'col-2 col-md-1 hour text-center py-3');
+    if(isPM){
+      if(i === 12){
+        $time.text(`${i} PM`);
+      }else{
+        $time.text(`${i} AM`);
+      }
+    }else{
+      if(i === 24){
+        $time.text(`${i-12} AM`);
+      }else{
+        $time.text(`${i-12} PM`);
+      }
+    }
+    let $textArea = $('<textarea class="col-8 col-md-10 description" rows="3"> </textarea');
+    let $saveBtn = $('<button>');
+    $saveBtn.attr('class', 'btn saveBtn col-2 col-md-1');
+    $saveBtn.attr('aria-label', 'save');
+    let $btnIcon = $('<i>');
+    $btnIcon.attr('class', 'fas fa-save');
+    if (i >= currentHour){
+      $btnIcon.attr('aria-hidden', 'true');
+    }else{
+      $btnIcon.attr('aria-hidden', 'false');
+    }
+    
+    $saveBtn.append($btnIcon);
+    $hour.append($time, $textArea, $saveBtn)
+    $timesheet.append($hour);
+  }
+  
+  // <div id="hour-9" class="row time-block past">
+  //    <div class="col-2 col-md-1 hour text-center py-3">9AM</div>
+  //    <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+  //    <button class="btn saveBtn col-2 col-md-1" aria-label="save">
+  //      <i class="fas fa-save" aria-hidden="true"></i>
+  //    </button>
+  // </div>
+
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
